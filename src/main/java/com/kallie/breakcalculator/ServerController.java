0c443b28b516b65cf55f86c504620a8a5f9ba55d
Kallie Yang <143293245+k-iwii@users.noreply.github.com>
2025-07-22 20:39:41 -0400

    trying to make heroku work 3

--- src/main/java/com/kallie/breakcalculator/ServerController.java
+++ src/main/java/com/kallie/breakcalculator/ServerController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -211,5 +212,14 @@ public class ServerController {
         }
 
         return breakResults;
+    }
+}
+
+@Controller
+class WebController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "forward:/landing.html";
     }
 }
